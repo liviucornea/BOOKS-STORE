4d22e2c295f1d69af0dcab176e3c08434f7e2cc2
Liviu Cornea <liviucornea@rogers.com>
2017-03-23 21:18:41 -0400

    In progress changes for ngrxStore

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationComponent } from './components/notification/notification.component';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NotificationComponent, NotAuthorizedComponent]
 })
 export class SharedModule { }
